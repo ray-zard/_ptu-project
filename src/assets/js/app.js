7ceaf4a3392931dc5d169d149822a988f413bbe2
ray-zard <ray-zard@mail.ru>
2016-10-21 11:23:05 +0400

    12345

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,11 @@
-$(document).foundation();
+$(document).ready(function() {
+
+  $(document).foundation();
+
+  $('.grid').isotope({
+    // options
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows'
+  });
+
+});
