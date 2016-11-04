2861d5df80ead88e6dd6fccb2e9c58c0ed01e627
ray-zard <ray-zard@mail.ru>
2016-11-04 22:47:54 +0400

    Save много чего

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -27,6 +27,31 @@ $(document).ready(function() {
     $grid.isotope('layout');
   });
 
+  var $newsGrid = $('.news-grid').isotope({
+    // options
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    masonry: {
+      columnWidth: '.grid-sizer'
+    }
+  });
+
+  $newsGrid.on( 'click', '.grid-item', function() {
+
+    // change size of item by toggling gigante class
+    if ( $(this).hasClass('large-4') ) {
+      $( '.grid-item.x-large-4' ).not(this).removeClass('x-large-4 x-large-8');
+      $( '.grid-item.large-12' ).not(this).removeClass('x-large-8 large-12').addClass('large-8');
+      $(this).addClass('x-large-4');
+    }
+    if ( $(this).hasClass('large-8') ) {
+      $( '.grid-item.large-8' ).not(this).removeClass('large-8 x-large-4');
+      $( '.grid-item.x-large-4' ).not(this).removeClass('x-large-4');
+      $(this).addClass('x-large-8 large-12').removeClass('large-8');
+    }
+    $newsGrid.isotope('layout');
+  });
+
 
 	$('.logo-line').slick({
 		arrows: false,
