f740f8b5e00e3fe0fbe35750733b01c79540623f
ray-zard <ray-zard@mail.ru>
2016-11-05 06:36:13 +0400

    Блок новости оформлен

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -29,27 +29,11 @@ $(document).ready(function() {
 
   var $newsGrid = $('.news-grid').isotope({
     // options
-    itemSelector: '.grid-item',
+    itemSelector: '.news-grid-item',
     percentPosition: true,
     masonry: {
-      columnWidth: '.grid-sizer'
+      columnWidth: '.news-grid-sizer'
     }
-  });
-
-  $newsGrid.on( 'click', '.grid-item', function() {
-
-    // change size of item by toggling gigante class
-    if ( $(this).hasClass('large-4') ) {
-      $( '.grid-item.x-large-4' ).not(this).removeClass('x-large-4 x-large-8');
-      $( '.grid-item.large-12' ).not(this).removeClass('x-large-8 large-12').addClass('large-8');
-      $(this).addClass('x-large-4');
-    }
-    if ( $(this).hasClass('large-8') ) {
-      $( '.grid-item.large-8' ).not(this).removeClass('large-8 x-large-4');
-      $( '.grid-item.x-large-4' ).not(this).removeClass('x-large-4');
-      $(this).addClass('x-large-8 large-12').removeClass('large-8');
-    }
-    $newsGrid.isotope('layout');
   });
 
 
