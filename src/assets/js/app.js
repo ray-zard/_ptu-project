802a9729bfd968445c9978a9f10f285c7799fef9
ray-zard <ray-zard@mail.ru>
2016-11-09 08:18:58 +0400

    Оформил форму

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -66,4 +66,21 @@ $(document).ready(function() {
     noMouse : true,
   });
 
+  //E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Спасибо!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
 });
